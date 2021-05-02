38b0b84a3d266f5548e140672eaf422fc7c96e83
Iftakher99 <19tonmoy99@gmail.com>
2021-05-03 02:09:04 +0600

    initial

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB23eTHBXtn2eG31D8hZRU5FUCBj_FV9DY",
+    authDomain: "portfolio-9ee5d.firebaseapp.com",
+    projectId: "portfolio-9ee5d",
+    storageBucket: "portfolio-9ee5d.appspot.com",
+    messagingSenderId: "218533942551",
+    appId: "1:218533942551:web:27ee3b1eb8bdc8ec235352"
+  };
+  export default firebaseConfig
